9b9fe2702dd05f20746a118a2cf091fce42485ce
joeyanovich <joedison.dias@gmail.com>
2024-02-18 22:40:45 -0300

    style: details in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,9 @@ import logo from '../../assets/logo.svg'
 
 export function Header() {
   return (
-    <header className='flex justify-between m-auto max-w-[100vw] px-20 py-10 bg-green-400'>
+    <header className='flex justify-between m-auto max-w-[100vw] px-20 py-10 mt-10'>
       <img src={ logo } alt="Huddle" />
-      <button className='bg-red-600'>Try It Free</button>
+      <button className='bg-transparnet rounded-full border-solid border-2 px-10 border-pink text-pink'>Try It Free</button>
     </header>
   )
 }

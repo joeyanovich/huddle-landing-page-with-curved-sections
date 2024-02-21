9d1a6b3818985f89d77fd70505e4c3e238e67626
joeyanovich <joedison.dias@gmail.com>
2024-02-20 21:48:53 -0300

    feat: create Button component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,15 +1,17 @@
+import { Button } from "../Button/Button";
+
 export function Banner() {
   return (
     <section className="flex flex-col items-center justify-center gap-16 py-10 h-[100vh]">
       <h1 className="text-4xl font-title text-dark-cyan">
         Build The Community Your Fans Will Love
       </h1>
-      <p className="max-w-[450px] text-center font-body text-dark-cyan">
+      <p className="max-w-[480px] text-center font-body text-dark-cyan">
         Huddle re-imagines the way build communities. You have a 
         voice, but so does your audience. Create connections with your
         users as you engage in genuine discussion.
       </p>
-      {/* <button>Get Started For Free</button> */}
+      <Button />
     </section>
   )
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+export function Button() {
+  return (
+    <button className="py-3 px-20 border-solid border-2 rounded-full border-pink bg-pink text-pale-blue">
+      Get Started For Free 
+    </button>
+  )
+}

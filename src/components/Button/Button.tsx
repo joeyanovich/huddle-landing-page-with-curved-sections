dfeb4c0af12dfdaf483a414af384d4ce9ebae8d5
joeyanovich <joedison.dias@gmail.com>
2024-02-20 21:56:22 -0300

    style: hover in Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 export function Button() {
   return (
-    <button className="py-3 px-20 border-solid border-2 rounded-full border-pink bg-pink text-pale-blue">
+    <button className="py-4 px-20 rounded-full font-semibold hover:px-[4.5rem] ease-in-out duration-500 bg-pink text-pale-blue hover:bg-light-pink">
       Get Started For Free 
     </button>
   )

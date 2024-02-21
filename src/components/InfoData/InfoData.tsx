b0a7deb36a1746097fa57ff0a75dd5ee353e00d1
joeyanovich <joedison.dias@gmail.com>
2024-02-20 22:30:24 -0300

    feat: create InfoNumbers component

--- src/components/InfoData/InfoData.tsx
+++ src/components/InfoData/InfoData.tsx
@@ -1,7 +1,14 @@
+import img from '../../assets/screen-mockups.svg'
+import { InfoNumbers } from '../InfoNumbers/InfoNumbers'
+
 export function InfoData() {
   return (
-    <div className="mt-96">
-      Hello World
-    </div>
+    <section className="flex flex-col items-center justify-center gap-20 mb-[1000px]">
+      <img src={ img } alt="" />
+      <div className='flex'>
+        <InfoNumbers />
+        {/* <InfoNumbers /> */}
+      </div>
+    </section>
   )
 }
---
+++ src/components/InfoNumbers/InfoNumbers.tsx
@@ -0,0 +1,6 @@
+export function InfoNumbers() {
+  return (
+    <div className="border-2">
+    </div>
+  )
+}

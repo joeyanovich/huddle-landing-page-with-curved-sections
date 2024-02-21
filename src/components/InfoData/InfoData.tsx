249415437ba846706e15266574506dbad5d387cc
joeyanovich <joedison.dias@gmail.com>
2024-02-21 00:05:48 -0300

    style: InfoNumbers component

--- src/components/InfoData/InfoData.tsx
+++ src/components/InfoData/InfoData.tsx
@@ -1,13 +1,16 @@
 import img from '../../assets/screen-mockups.svg'
 import { InfoNumbers } from '../InfoNumbers/InfoNumbers'
 
+import communities from '../../assets/icon-communities.svg'
+import messages from '../../assets/icon-messages.svg'
+
 export function InfoData() {
   return (
     <section className="flex flex-col items-center justify-center gap-20 mb-[1000px]">
       <img src={ img } alt="" />
-      <div className='flex'>
-        <InfoNumbers />
-        {/* <InfoNumbers /> */}
+      <div className='flex justify-between max-w-[62.5rem] w-full'>
+        <InfoNumbers icon={ communities } number='1.4k+' text='Communities Formed' />
+        <InfoNumbers icon={ messages } number='2.7m+' text='Messages Sent' />
       </div>
     </section>
   )
--- src/components/InfoNumbers/InfoNumbers.tsx
+++ src/components/InfoNumbers/InfoNumbers.tsx
@@ -1,6 +1,15 @@
-export function InfoNumbers() {
+interface InfoNumbersProps {
+  icon: string
+  number: string
+  text: string
+}
+
+export function InfoNumbers({ icon, number, text }: InfoNumbersProps) {
   return (
-    <div className="border-2">
+    <div className="flex flex-col gap-5 p-8">
+      <img src={icon} className='w-12' />
+      <h2 className='font-body font-bold text-8xl'>{number}</h2>
+      <p className='font-body text-2xl text-center text-gray-400'>{text}</p>
     </div>
   )
 }

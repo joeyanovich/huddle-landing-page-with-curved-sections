44dec0688b19cfc34d427c3da43f54495bf8ef64
joeyanovich <joedison.dias@gmail.com>
2024-02-21 22:37:06 -0300

    style: set benefits section size

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -2,14 +2,16 @@ import img from '../../assets/illustration-grow-together.svg'
 
 export function Benefits() {
   return (
-    <section className='flex'>
-      <div>
-        <h3>Grow Together</h3>
-        <p>Generate meaningful discussions with your audience and build a
+    <section className='flex items-center content-center w-full max-w-[1280px] m-auto bg-slate-500'>
+      <div className='bg-green-500 flex flex-col gap-8 w-full'>
+        <h3 className='font-title font-bold text-4xl'>Grow Together</h3>
+        <p className='font-body'>Generate meaningful discussions with your audience and build a
           strong, loyal community. Think of the insightful conversations you
           miss out on with a feedback form. </p>
       </div>
-      <img src= {img } alt="" />
+      <div className=' w-full bg-red-500'>
+        <img className='' src= {img } alt="" />
+      </div>
     </section>
   )
 }
